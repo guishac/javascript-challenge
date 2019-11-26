fad8216aaca50856d9c2f89b5e04bd961d95f2fb
guishac <52980433+guishac@users.noreply.github.com>
2019-11-26 17:42:50 -0500

    Add files via upload

--- UFO level 1/static/js/app.js
+++ UFO level 1/static/js/app.js
@@ -2,36 +2,41 @@
 var tableData = data;
 
 // YOUR CODE HERE!
-var tbody = d3.select("tbody");
-
-var button = d3.select("#button");
-console.log(tableData);
-
-data.forEach(function (ufoSighting) {
-    console.log(ufoSighting);
-    var row = tbody.append("tr");
 
 
-    Object.entries(ufoSighting).forEach(function ([key, value]) {
-        console.log(key, value);
-        var cell = row.append("td");
-        cell.text(value);
+//var button = d3.select("#button");
+console.log(tableData)
+
+date(tableData)
+function date(datedata) {
+    var tbody = d3.select("tbody");
+    tbody.html('')
+    datedata.forEach(function (ufoSighting) {
+        console.log(ufoSighting);
+        var row = tbody.append("tr");
 
 
-        
+        Object.entries(ufoSighting).forEach(function ([key, value]) {
+            console.log(key, value);
+            var cell = row.append("td");
+            cell.text(value);
 
-
-        button.on("click", function () {
-            d3.event.preventDefault();
-            var inputElement = d3.select("#datetime");
-            var inputValue = inputElement.property("value");
-            console.log(inputValue);
-            console.log(tableData);
-            var filteredData = tableData.filter(data => data.datetime === inputValue);
-            console.log(filteredData);
         });
+
     });
+}
+var submit = d3.select("#filter-btn")
+submit.on("click", function () {
+    d3.event.preventDefault();
+    var inputElement = d3.select("#datetime");
+    var inputValue = inputElement.property("value");
+    console.log(inputValue);
+    console.log(tableData);
+    var filteredData = tableData.filter(tabledata => tabledata.datetime === inputValue);
+    console.log(filteredData);
+    date(filteredData);
 });
+    
 
 
 
